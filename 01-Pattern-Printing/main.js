d5dc4bb321aaa710df5a6ef7da4b2e8f62438f7e
itscsp <chethanspoojary1@gmail.com>
2024-08-31 12:35:37 +0530

    Pratice Number 1

--- 01-Pattern-Printing/main.js
+++ 01-Pattern-Printing/main.js
@@ -1,2 +1,19 @@
-console.log("Hello World...	")
-console.log("Testing...")
+/**
+ Pattern Number 1:
+ 1. ******
+    ******
+    ******
+    ******
+    ******
+ */
+
+const pattern01 = function(n){
+        console.log("Pattern Number 1: \n")
+        for(let row = 0; row <= n; row++){
+            console.log("******")
+        }
+    
+    }
+    
+    
+    pattern01(5);

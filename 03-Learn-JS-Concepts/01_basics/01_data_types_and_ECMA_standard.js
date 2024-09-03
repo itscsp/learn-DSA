a4c0e48418b920bb2dfab32cfdf613115d68c4d3
itscsp <chethanspoojary1@gmail.com>
2024-09-03 23:06:21 +0530

    Data types in JS

---
+++ 03-Learn-JS-Concepts/01_basics/01_data_types_and_ECMA_standard.js
@@ -0,0 +1,15 @@
+let i_am_variable = 100
+console.log(i_am_variable, "| My Type:", typeof i_am_variable)
+
+i_am_variable = "Namaste JavaScript" 
+console.log(i_am_variable, "| My Type:", typeof i_am_variable)
+
+
+i_am_variable = true 
+console.log(i_am_variable, "| My Type:", typeof i_am_variable)
+
+/**
+ * JS ia weakly type language
+ * Which mean user can assign any value to variable. any variable can be assigned (and re-assigned) values of all types
+ * 
+ */

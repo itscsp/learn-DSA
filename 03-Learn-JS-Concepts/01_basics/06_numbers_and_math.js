db7970ef07b967d52e349572e30cbb5d36daaecc
itscsp <chethanspoojary1@gmail.com>
2024-09-08 11:10:46 +0530

    Math in JS

--- 03-Learn-JS-Concepts/01_basics/06_numbers_and_math.js
+++ 03-Learn-JS-Concepts/01_basics/06_numbers_and_math.js
@@ -19,9 +19,20 @@ let afterFixed = number2.toFixed(2);
  * Math in JavaScript
  */
 
-console.log(Math)
-console.log("abs in js:",Math.abs(-100)) //conver negative value into positive
-console.log("round in js:",Math.round(100.6)) // Round into in js
-console.log("Ceil in js:",Math.ceil(100.2)) // Round into in js
-console.log("Floor in js:",Math.floor(100.6)) // Round into in js
+// console.log(Math)
+// console.log("abs in js:",Math.abs(-100)) //conver negative value into positive
+// console.log("round in js:",Math.round(100.6)) // Round into in js
+// console.log("Ceil in js:",Math.ceil(100.2)) // Round into in js
+// console.log("Floor in js:",Math.floor(100.6)) // Round into in js
+
+
+// console.log("Rondom Values in JS:", Math.floor((Math.random() * 10 + 1)))
+
+// Get randon value from min to max
+const min = 50;
+const max = 100;
+
+let randomValue = Math.random() * (max - min + 1) + min
+
+console.log(Math.floor(randomValue))
 

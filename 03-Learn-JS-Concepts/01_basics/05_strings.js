4442ad873639690d755c685989ccc97460a422c5
itscsp <chethanspoojary1@gmail.com>
2024-09-08 09:03:14 +0530

    Basics

---
+++ 03-Learn-JS-Concepts/01_basics/05_strings.js
@@ -0,0 +1,13 @@
+/**
+ * STRING AND METHODS IN JAVASCRIPT
+ */
+
+// We can define 2 types
+let string1 = "HelliBased in India, I'm a web developer passionate about building accessible web apps that users love."
+let string2 = new String("Hello Based in India, I'm a web developer passionate about building accessible web apps that users love.")
+
+// Replace Hello with Hi
+console.log(string2.replace("Hello", "Hi")); //Output: Hi Based in India, I'm a web developer passionate about building accessible web apps that users love.
+console.log(string2.replaceAll('a', 'xyz')); //Output: Hello Bxyzsed in Indixyz, I'm xyz web developer pxyzssionxyzte xyzbout building xyzccessible web xyzpps thxyzt users love.
+
+// There are many methods
---
+++ 03-Learn-JS-Concepts/01_basics/06_numbers_and_math.js
@@ -0,0 +1,27 @@
+/**
+ * Numbers in JavaScript
+ */
+
+// 2 ways to define js 
+let number1 = 1000000
+let number2 = new Number(number1)
+
+// console.log(number1);
+// console.log(number2);
+
+// console.log(number2.toFixed(2))
+
+let afterFixed = number2.toFixed(2);
+// console.log(number2.toLocaleString("en-IN")+".00")
+
+
+/**
+ * Math in JavaScript
+ */
+
+console.log(Math)
+console.log("abs in js:",Math.abs(-100)) //conver negative value into positive
+console.log("round in js:",Math.round(100.6)) // Round into in js
+console.log("Ceil in js:",Math.ceil(100.2)) // Round into in js
+console.log("Floor in js:",Math.floor(100.6)) // Round into in js
+

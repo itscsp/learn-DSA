06110e20f9af6364da37f825b8815ddf32276250
itscsp <chethanspoojary1@gmail.com>
2024-09-11 09:27:52 +0530

    Array in JavaScript

---
+++ 03-Learn-JS-Concepts/02_basics/01_array.js
@@ -0,0 +1,13 @@
+/**
+ * Array in JavaScript
+ */
+
+let newArray1 = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+let newArray2 = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+let newArray3 = ["a", "b", "c", "d"]
+
+
+console.log("Array example 1: ",newArray1)
+console.log("Array example 2: ",newArray2)
+console.log("Array example 3: ",newArray2)
+

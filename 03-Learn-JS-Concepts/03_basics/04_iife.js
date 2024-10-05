954aa269e0d493d07ee3f6e4cb5eef409b2eb682
itscsp <chethanspoojary1@gmail.com>
2024-10-05 19:22:24 +0530

    IIFE functions

---
+++ 03-Learn-JS-Concepts/03_basics/04_iife.js
@@ -0,0 +1,15 @@
+
+/**
+ * IIFE: Immediately Invoked Function Expressions
+ */
+
+// Syntax
+(function(){
+    //Code goes here.
+})(); //Here we need to add ; becouse this will give error for next function
+
+
+// Some Developer do add ";" at begining of the function. Like below code.
+;(function(){    
+    console.log("Hello")
+})()

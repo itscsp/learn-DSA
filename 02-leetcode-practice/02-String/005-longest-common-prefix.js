51d1c83525cc29048405cee2959e738605b7ad3e
itscsp <chethanspoojary1@gmail.com>
2024-11-05 00:47:07 +0530

    Longest-common-prefix

---
+++ 02-leetcode-practice/02-String/005-longest-common-prefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+
+    let firstLetter = strs[0].split("")
+let i=1
+    let currentLetter = strs[i]
+    console.log(firstLetter[i])
+    console.log(currentLetter[i])
+    // for(let i=1; i<firstLetter.length; i++){
+    //     if(firstLetter[i] == currentLetter[i]){
+
+    //     }
+    // }
+};
+
+longestCommonPrefix(["flower","flow","flight"])

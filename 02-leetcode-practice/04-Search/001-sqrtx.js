f2d5e6cd35e8292faa7a88e2b32efdd1b398542b
itscsp <chethanspoojary1@gmail.com>
2024-11-06 21:23:29 +0530

    Searching alogoritham

--- 02-leetcode-practice/04-Search/001-sqrtx.js
+++ 02-leetcode-practice/04-Search/001-sqrtx.js
@@ -2,19 +2,18 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x = 4) {
-    if (x == 0) {
-        return x;
-    }
-    if (x == 1) {
-        return x;
-    }
+var mySqrt = function (x) {
+    if (x == 0 || x==1) {
+       return x;
+   }
 
-    for(let i = 0; i < x; i++){
-        if(i*i == x){
-            return i;
-        }
-    }
+   for(let i = 0; i <= x; i++){
+       if(i*i == x){
+           return i;
+       }
+
+       if(i*i > x){
+           return i-1
+       }
+   }
 };
-
-mySqrt()

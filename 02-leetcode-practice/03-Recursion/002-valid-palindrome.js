147adbd362f4f4c6041b4ab98ffa3a317f4acc10
itscsp <chethanspoojary1@gmail.com>
2024-11-06 07:39:16 +0530

    Leetcode question is palidrome or not

--- 02-leetcode-practice/03-Recursion/002-valid-palindrome.js
+++ 02-leetcode-practice/03-Recursion/002-valid-palindrome.js
@@ -3,7 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    console.log(s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase())
+    let filteredString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    if(filteredString.length <= 1){
+        return true;
+    }
+    if(filteredString[0] !== filteredString[filteredString.length-1]){
+        return false
+    }
+    return isPalindrome(filteredString.slice(1,-1))
+    
 };
 
-isPalindrome("A man, a plan, a canal: Panama")
+console.log(isPalindrome("A man, a plan, a canal: Panama"))

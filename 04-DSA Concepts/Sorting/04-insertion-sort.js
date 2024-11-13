55bf429e0d6bfa8912b4c7194f7901d2332c0f88
itscsp <chethanspoojary1@gmail.com>
2024-11-13 21:53:54 +0530

    insertion sort

---
+++ 04-DSA Concepts/Sorting/04-insertion-sort.js
@@ -0,0 +1,16 @@
+const insertionSort = (arr) => {
+    for(let i = 1; i < arr.length; i++ ){
+        let currentElement = arr[i];
+        let j = i-1
+
+        while(j >= 0 && arr[j] > currentElement){
+            arr[j+1] = arr[j]
+            j--
+        }
+        arr[j+1] = currentElement
+    }
+
+    return arr
+}
+
+console.log(insertionSort([-2,5,1000, -7]))

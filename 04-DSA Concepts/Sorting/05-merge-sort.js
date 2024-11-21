e20dd2529b3d280c68d2a2feae0952f7d3d23b70
itscsp <chethanspoojary1@gmail.com>
2024-11-21 06:43:33 +0530

    Merge sort optimized solution

--- 04-DSA Concepts/Sorting/05-merge-sort.js
+++ 04-DSA Concepts/Sorting/05-merge-sort.js
@@ -49,5 +49,50 @@ let merge = (left, right) => {
 
     return result;
 }
-mergeSort([29, 10, 8, 16, 37, 14, 45, 4])
+// mergeSort([29, 10, 8, 16, 37, 14, 45, 4])
 // console.log(...mergeSort([29, 10, 8, 16, 37, 14, 45, 4]))
+
+
+/**
+ * Optimized solution
+ */
+const mergeSortInplace = (arr, low, high) => {
+    if(low < high) {
+        let mid = Math.floor((low + high) / 2);
+        mergeSortInplace(arr, low, mid); // left side
+        mergeSortInplace(arr, mid+1, high); // right side
+        mergeInplace(arr, low, mid, high); // Merge
+    }
+}
+
+const mergeInplace = (arr, low, mid, high) =>{
+    const result = []
+    let leftIndex = low,  rightIndex = mid+1;
+    while (leftIndex <= mid && rightIndex <= high) {
+        if(arr[leftIndex] < arr[rightIndex]) {
+            result.push(arr[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(arr[rightIndex])
+            rightIndex++
+        }
+    }
+
+    while (leftIndex <= mid) {
+        result.push(arr[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex <= high) {
+        result.push(arr[rightIndex])
+        rightIndex++
+    }
+
+    for(let i = low; i<=high; i++){
+        arr[i] = result[i-low];
+    }
+}
+
+let arr = [29, 10, 8, 16, 37, 14, 45, 4]
+mergeSortInplace(arr, 0, arr.length - 1)
+console.log(arr)
